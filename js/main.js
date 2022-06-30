b26a20ca6460b52491d0d98510a23aab46b81383
Anastasia Ostapenko <ostapenko.nastya.ao@gmail.com>
2022-06-30 12:12:48 +0300

    load all files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.bike-slider, .slider__item').slick({
+        arrows:false,
+        dots:true,
+        fake: true,
+        autoplay:true,
+        autoplaySpeed:2000,
+    });
+
+});
